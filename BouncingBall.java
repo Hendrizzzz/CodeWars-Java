e24b9eeeeec77678680d39c70606b2d2b34bfdb1
Hendrixx <139997209+Hendrizzzz@users.noreply.github.com>
2024-01-17 20:02:39 +0800

    Create BouncingBall.java

---
+++ BouncingBall.java
@@ -0,0 +1,34 @@
+/*
+DESCRIPTION:
+Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted).
+
+Examples:
+
+Input -> Output
+1,2,2 -> true
+4,2,3 -> true
+2,2,2 -> true
+1,2,3 -> false
+-5,1,3 -> false
+0,2,3 -> false
+1,2,9 -> false 
+*/
+
+
+public class BouncingBall {
+	
+	public static int bouncingBall(double h, double bounce, double window) {
+        if(h <= 0) return -1;
+        if(bounce <= 0 || bounce >= 1) return -1;
+        if(window > h) return -1;
+
+        int bounceCount = 0;
+        while(h > window){
+            bounceCount++;
+            h *= bounce;
+        }
+        return bounceCount*2 - 1;
+    }
+}
